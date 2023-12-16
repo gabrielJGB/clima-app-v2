1b8a406878261a691da3e7de11c571512c6343ff
gabriel <brunengo.gabriel@gmail.com>
2023-12-16 07:40:11 -0300

    update

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,7 +1,9 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import { useContext } from 'react';
+import { useLocation, useNavigate } from 'react-router-dom';
 import magnifier from '../../assets/magnifier.png'
 import star from '../../assets/star.png'
+import back_button from '../../assets/back_button.png'
 // import star_outline from '../../assets/star-outline.png'
 
 import { DataContext } from '../../context/DataContext';
@@ -9,25 +11,67 @@ import './Header.scss'
 
 const Header = () => {
   const data = useContext(DataContext)
+  const location = useLocation()
+  const navigate = useNavigate()
+  const { pathname } = location;
   const ICON_SIZE = 30;
+  const ICON_SIZE_2 = 24;
+
+  useEffect(() => {
+
+    console.log(pathname);
+  }, [pathname])
+
 
   return (
     <div className='header-container'>
-            <div  className="favorites-button" 
-            onClick={()=>data.set_show_favorites(prev=>prev?false:true)} 
-      >
 
-        <img src={star} width={ICON_SIZE} height={ICON_SIZE} />
-      </div>
 
-      <div className='title'>{data.selected_city?"El tiempo en "+data.selected_city.nombre:"Pronóstico del tiempo"}</div>
+      {
+        pathname === "/" &&
+        <div className="favorites-button"
+          onClick={() => data.set_show_favorites(prev => prev ? false : true)}
+        >
 
-      <div  className="search-button" 
-            onClick={()=>data.set_show_search_modal(prev=>prev?false:true)} 
-      >
+          <img src={star} width={ICON_SIZE} height={ICON_SIZE} />
+        </div>
+      }
 
-        <img src={magnifier} width={ICON_SIZE} height={ICON_SIZE} />
-      </div>
+
+
+      {
+        pathname === "/day" &&
+        <div className="back-button"
+          onClick={() => { navigate("/") }}
+        >
+
+
+          <img src={back_button} width={ICON_SIZE_2} height={ICON_SIZE_2} />
+        </div>
+      }
+
+
+
+
+      <div className='title'>{data.selected_city ? "El tiempo en " + data.selected_city.nombre : "Pronóstico del tiempo"}</div>
+
+      {
+        pathname === "/" &&
+        <div className="search-button"
+          onClick={() => data.set_show_search_modal(prev => prev ? false : true)}
+        >
+
+          <img src={magnifier} width={ICON_SIZE} height={ICON_SIZE} />
+        </div>
+      }
+
+
+
+      {
+        pathname === "/day" &&
+        <div></div>
+      }
+
     </div>
   )
 }
