b35a2b555b6294aed82519f037b0e5b40c26607f
gabriel <brunengo.gabriel@gmail.com>
2024-01-09 07:10:27 -0300

    update

--- dev-dist/sw.js
+++ dev-dist/sw.js
@@ -82,7 +82,7 @@ define(['./workbox-b5f7729d'], (function (workbox) { 'use strict';
     "revision": "3ca0b8505b4bec776b69afdba2768812"
   }, {
     "url": "index.html",
-    "revision": "0.6usa2qgo798"
+    "revision": "0.ajpj4e5v6j8"
   }], {});
   workbox.cleanupOutdatedCaches();
   workbox.registerRoute(new workbox.NavigationRoute(workbox.createHandlerBoundToURL("index.html"), {
